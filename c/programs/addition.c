8e106e7742f838a2b9a0c05d597e714440f1069d
Divyansh Verma <vdivyansh27@gmail.com>
2026-01-22 22:11:46 +0530

    addition code add

--- c/programs/addition.c
+++ c/programs/addition.c
@@ -9,3 +9,50 @@ int main()
     printf("%d + %d = %d", x, y, (x + y));
     return 0;
 }
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int x, y;
+    char ch;
+    do
+    {
+        printf("enter first number: ");
+        scanf("%d", &x);
+        printf("enter second number: ");
+        scanf("%d", &y);
+        printf("%d + %d = %d", x, y, (x + y));
+
+        getchar();
+        printf("\n\nConitune? y/n :");
+        ch = getche();
+        printf("\n");
+    } while (ch == 'Y' || ch == 'y');
+    return 0;
+}
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int num, sum = 0, flag = 1;
+    printf("enter '=' to cal\n");
+    printf("enter values : \n");
+    printf("0 + ");
+    while (flag != 0)
+    {
+        flag = scanf("%d", &num);
+        if (flag == 1)
+        {
+            getchar();
+            sum += num;
+            printf("%d + ", sum);
+        }
+        else
+        {
+            flag = 0;
+            printf("total : %d", sum);
+        }
+    }
+}
